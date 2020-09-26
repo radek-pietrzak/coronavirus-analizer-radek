8cd28080b2380104448e71426a17a626aeda749c
radek <radekpietrzak102@gmail.com>
2020-09-26 10:17:23 +0200

    FX3 Znajdź sposób, jak klikając przycisk JavyFX otworzyć nowe okno. Utwórz przykład i umieść w projekcie. Stworznie metody showGraph w klasie MainUI, który będzie tworzył nową scenę

--- src/main/java/pl/sdacademy/javafx/MainUI.java
+++ src/main/java/pl/sdacademy/javafx/MainUI.java
@@ -1,6 +1,7 @@
 package pl.sdacademy.javafx;
 
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -12,7 +13,7 @@ public class MainUI extends VBox {
     Label labelMainMenu;
     Label labelLastUpdate;
 
-//    FX2 stworzenie konstruktora, który wyświetli elementy z zadania
+    //    FX2 stworzenie konstruktora, który wyświetli elementy z zadania
     public MainUI(Stage stage) {
         super();
         buttonShowGraph = new Button("Wyświetl wykres");
@@ -23,8 +24,18 @@ public class MainUI extends VBox {
         this.getChildren().add(buttonRefreshData);
         this.getChildren().add(labelMainMenu);
         this.getChildren().add(labelLastUpdate);
-//        buttonShowGraph.setOnAction(event -> showGraph(stage));
+//        FX3 po kliknięciu tworzy się nowa scena
+        buttonShowGraph.setOnAction(event -> showGraph(stage));
         this.setAlignment(Pos.CENTER);
+    }
+
+    //    FX3 tworzenie metody, nowej sceny
+    private void showGraph(Stage stage) {
+        VBox graphVBox = new VBox();
+        Label topInfo = new Label("New graph here");
+        graphVBox.getChildren().add(topInfo);
+        Scene graphScene = new Scene(graphVBox, 600, 800);
+        stage.setScene(graphScene);
     }
 
 
